fa741314802073d063dd7b1c72583cd37a9d7032
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 14:29:52 -0500

    main server initial setup

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-Deno.serve(() => {
-  return new Response('hello world')
-})
+import { route, type Route } from '@std/http/unstable-route'
+import { STATUS_CODE } from '@std/http'
+
+const routes: Route[] = []
+
+Deno.serve(route(routes, () => new Response('Not Found', { status: STATUS_CODE.NotFound})))
