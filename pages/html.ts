a041cdd4f737ae0d1adaf36926007c679b8335c5
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 17:10:55 -0500

    reduced str

--- pages/html.ts
+++ pages/html.ts
@@ -2,8 +2,5 @@ import { escape } from "@std/html";
 
 export const html = (strings: TemplateStringsArray, ...values: string[]) =>
   strings.reduce(
-    (escaped, str, i) => {
-      console.log({ escaped, str, i });
-      return `${escaped}${escape(values[i - 1])}${str}`;
-    },
+    (escaped, str, i) => `${escaped}${escape(values[i - 1])}${str}`,
   );
