0fd7092a29fe0a8cdfcb60b31fc9a41753891133
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 14:36:36 -0500

    routes to http file

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { type Route, route } from "@std/http/unstable-route";
 import { STATUS_CODE } from "@std/http";
+import { serveDir } from "@std/http/file-server";
 
-const routes: Route[] = [];
+const routes: Route[] = [{
+  pattern: new URLPattern({ pathname: "/*" }),
+  handler: (req) =>
+    serveDir(req, {
+      fsRoot: ".",
+    }),
+}];
 
 Deno.serve(
   route(
