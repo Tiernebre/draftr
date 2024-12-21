dde3519fad614071bc51bad73c7317deb18eea73
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 16:25:08 -0500

    linking between pages

--- pages/drafts/mod.ts
+++ pages/drafts/mod.ts
@@ -1,29 +1,27 @@
 import { Route } from "@std/http/unstable-route";
 import { serveDir } from "@std/http/file-server";
-import { page } from "./layout.ts";
-import { routes as draftRoutes } from "./drafts/mod.ts";
+import { page } from "../layout.ts";
 
 export const routes: Route[] = [
   {
-    pattern: new URLPattern({ pathname: "/" }),
+    pattern: new URLPattern({ pathname: "/drafts" }),
     handler: () =>
       page({
-        body: `<h1>
-          Home</h1>
-          <p>
-            <a href="/drafts">Drafts</a>
-          </p>
+        body: `
+        <h1>Drafts</h1>
+        <p>
+            <a href="/">Home</a>
+        </p>
         `,
         head: `<link rel="stylesheet" href="./index.css">`,
-        title: "Draftr",
+        title: "Draftr | Drafts",
       }),
   },
   {
-    pattern: new URLPattern({ pathname: "/*.css" }),
+    pattern: new URLPattern({ pathname: "/drafts/*.css" }),
     handler: (req) =>
       serveDir(req, {
         fsRoot: import.meta.dirname,
       }),
   },
-  ...draftRoutes,
 ];
