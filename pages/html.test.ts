364ddf60e144b2257c54f130924d8099d8c4e1ed
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 17:00:19 -0500

    WIP html escaping funtimes

--- pages/html.test.ts
+++ pages/html.test.ts
@@ -4,3 +4,12 @@ import { html } from "./html.ts";
 Deno.test("templates an empty string", () => {
   assertEquals(html``, "");
 });
+
+Deno.test("templates a non tagged string", () => {
+  assertEquals(html`<h1>hello world</h1>`, "<h1>hello world</h1>");
+});
+
+Deno.test("templates a tagged string", () => {
+  const tag = "hello world";
+  assertEquals(html`<h1>${tag}</h1>`, `<h1>${tag}</h1>`);
+});
--- pages/html.ts
+++ pages/html.ts
@@ -1,4 +1,7 @@
 import { escape } from "@std/html";
 
-export const html = (strings: TemplateStringsArray, ...values: string[]) =>
-  strings.reduce((escaped, str, i) => `${escaped}${str}${escape(values[i])}`);
+export const html = (strings: TemplateStringsArray, ...values: string[]) => {
+  return strings.reduce((escaped, str, i) => {
+    return `${escaped}${str}${escape(values[i] || "")}`;
+  });
+};
