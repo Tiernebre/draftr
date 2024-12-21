ee9bb754d255272618707387db33db4309ef16f9
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 14:58:55 -0500

    routes all separated

--- src/routes/mod.ts
+++ src/routes/mod.ts
@@ -1,11 +1,8 @@
 import { Route } from "@std/http/unstable-route";
+import { routes as homeRoutes } from "../features/home/mod.ts";
 import { routes as draftRoutes } from "../features/draft/mod.ts";
-import { page } from "../html/mod.ts";
 
 export const routes: Route[] = [
+  ...homeRoutes,
   ...draftRoutes,
-  {
-    pattern: new URLPattern({ pathname: "*" }),
-    handler: (_req) => page("index"),
-  },
 ];
