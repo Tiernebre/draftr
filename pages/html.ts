70db53cd76e8d254c139c0bb02cdf9cae38545db
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 17:10:43 -0500

    workign escaped

--- pages/html.ts
+++ pages/html.ts
@@ -1,7 +1,9 @@
 import { escape } from "@std/html";
 
-export const html = (strings: TemplateStringsArray, ...values: string[]) => {
-  return strings.reduce((escaped, str, i) => {
-    return `${escaped}${str}${escape(values[i] || "")}`;
-  });
-};
+export const html = (strings: TemplateStringsArray, ...values: string[]) =>
+  strings.reduce(
+    (escaped, str, i) => {
+      console.log({ escaped, str, i });
+      return `${escaped}${escape(values[i - 1])}${str}`;
+    },
+  );
