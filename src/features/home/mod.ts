7127fb1e45aaf6830ee8702298183ddc55d3d8d9
Brendan Tierney <tiernebre@gmail.com>
2024-12-21 14:58:19 -0500

    home mod file

---
+++ src/features/home/mod.ts
@@ -0,0 +1,9 @@
+import { Route } from "@std/http/unstable-route";
+import { page } from "../../html/mod.ts";
+
+export const routes: Route[] = [
+  {
+    pattern: new URLPattern({ pathname: "/" }),
+    handler: (_req) => page("home"),
+  },
+];
