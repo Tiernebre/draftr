31efde5f786d60bb59d2b572f56aabc9e0f0db77
Brendan Tierney <tiernebre@gmail.com>
2024-12-29 00:35:53 +0000

    remove debug SQL log

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 import { route } from "@std/http/unstable-route";
 import { STATUS_CODE } from "@std/http";
 import { routes } from "./pages/mod.ts";
-import { sql } from "./lib/mod.ts";
-
-const [result] = await sql`SELECT 1`;
-console.log({ result });
 
 Deno.serve(
   route(
