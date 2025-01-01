dabe988437f427ee1959ba56a8173062343bf735
Brendan Tierney <tiernebre@gmail.com>
2025-01-01 23:51:36 +0000

    working setup

--- pages/accounts/mod.ts
+++ pages/accounts/mod.ts
@@ -18,9 +18,9 @@ export const routes: Route[] = [
         <h1>Accounts</h1>
         <form method="POST" action=".">
           <label for="username">Username</label>
-          <input id="username" name="username" type="text">
+          <input id="username" name="username" type="text" required>
           <label for="password">Password</label>
-          <input id="password" name="password" type="password">
+          <input id="password" name="password" type="password" required>
           <button>Create Account</button>
         </form>
         `,
