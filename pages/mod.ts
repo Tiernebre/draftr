77e2bda95b9e7a4595021adbe0c99a9ef84080d7
Brendan Tierney <tiernebre@gmail.com>
2025-01-04 23:29:43 +0000

    with session support

--- pages/mod.ts
+++ pages/mod.ts
@@ -3,21 +3,26 @@ import { serveDir } from "@std/http/file-server";
 import { page } from "./page.ts";
 import { routes as draftRoutes } from "./drafts/mod.ts";
 import { routes as accountRoutes } from "./accounts/mod.ts";
+import { withSession } from "./session.ts";
 
 export const routes: Route[] = [
   {
     pattern: new URLPattern({ pathname: "/" }),
-    handler: () =>
+    handler: withSession((_request, session) =>
       page({
         body: /* html */ `<h1>
           Home</h1>
           <p>
             <a href="/drafts/">Drafts</a>
           </p>
+          <p>
+            ${JSON.stringify(session)}
+          </p>
         `,
         head: /* html */ `<link rel="stylesheet" href="./index.css">`,
         title: "Draftr",
-      }),
+      })
+    ),
   },
   {
     pattern: new URLPattern({ pathname: "*.css" }),
--- pages/session.ts
+++ pages/session.ts
@@ -1,9 +1,23 @@
 import { getCookies } from "@std/http";
 import { selectSession } from "../lib/session.ts";
+import Session from "../types/db/public/Session.ts";
+import { Optional } from "../lib/optional.ts";
+import { Handler } from "@std/http/unstable-route";
 
 export const SESSION_COOKIE_NAME = "session";
 
 export const getSession = async (request: Request) => {
   const sessionId = getCookies(request.headers)[SESSION_COOKIE_NAME];
   if (sessionId) return await selectSession(sessionId);
+};
+
+export const withSession = (
+  callback: (
+    request: Request,
+    session?: Optional<Session>,
+  ) => Response | Promise<Response>,
+): Handler =>
+async (request: Request) => {
+  const session = await getSession(request);
+  return callback(request, session);
 };
