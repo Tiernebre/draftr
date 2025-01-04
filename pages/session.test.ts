695868388a70c931b4417c1ca31b3daad4dbc8b5
Brendan Tierney <tiernebre@gmail.com>
2025-01-04 23:32:31 +0000

    session handling

--- pages/session.test.ts
+++ pages/session.test.ts
@@ -1,5 +1,5 @@
-import { assert, assertFalse } from "@std/assert";
-import { getSession, SESSION_COOKIE_NAME } from "./session.ts";
+import { assert, assertEquals, assertFalse } from "@std/assert";
+import { getSession, SESSION_COOKIE_NAME, withSession } from "./session.ts";
 import { randomUUID } from "node:crypto";
 import { createAccount } from "../lib/account.ts";
 import { insertSession } from "../lib/session.ts";
@@ -23,5 +23,36 @@ Deno.test("gets a session", async () => {
   });
   const gottenSession = await getSession(request);
   assert(gottenSession);
+  await sql.end();
+});
+
+Deno.test("can extend a session onto a route handler", async () => {
+  const { person_id } = await createAccount({
+    username: `username-${randomUUID()}`,
+    password: "password",
+  });
+  const session = await insertSession(person_id);
+  const request = new Request("http://0.0.0.0", {
+    headers: {
+      "Cookie": `${SESSION_COOKIE_NAME}=${session.id}`,
+    },
+  });
+  const response = await withSession((_req, session) =>
+    new Response(JSON.stringify(session))
+  )(
+    request,
+  );
+  assertEquals(await response.text(), JSON.stringify(session));
+  await sql.end();
+});
+
+Deno.test("can extend lack of a session onto a route handler", async () => {
+  const request = new Request("http://0.0.0.0");
+  const response = await withSession((_req, session) =>
+    new Response(JSON.stringify(session))
+  )(
+    request,
+  );
+  assertEquals(await response.text(), "");
   await sql.end();
 });
