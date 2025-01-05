53e08c0cdd2e762fd73fa41422339b265c206d03
Brendan Tierney <tiernebre@gmail.com>
2025-01-05 23:34:56 +0000

    figuring out hashing

--- lib/account.test.ts
+++ lib/account.test.ts
@@ -3,18 +3,35 @@ import { CreateAccountRequest } from "../types/dto/account.ts";
 import { sql } from "./sql.ts";
 import { randomUUID } from "node:crypto";
 import { assert, assertNotEquals } from "@std/assert";
-import { createAccount } from "./account.ts";
+import { createAccount, getAccount } from "./account.ts";
+import { afterAll, describe, it } from "@std/testing/bdd";
 
-Deno.test("creates an account", async () => {
-  const request: CreateAccountRequest = {
-    username: `username-${randomUUID()}`,
-    password: "password",
-  };
-  const account = await createAccount(request);
-  assert(account.id);
-  assert(account.person_id);
-  assertEquals(account.username, request.username);
-  assert(account.password);
-  assertNotEquals(account.password, request.password);
-  await sql.end();
+describe("account", () => {
+  afterAll(async () => {
+    await sql.end();
+  });
+
+  it("creates an account", async () => {
+    const request: CreateAccountRequest = {
+      username: `username-${randomUUID()}`,
+      password: "password",
+    };
+    const account = await createAccount(request);
+    assert(account.id);
+    assert(account.person_id);
+    assertEquals(account.username, request.username);
+    assert(account.password);
+    assertNotEquals(account.password, request.password);
+  });
+
+  it("gets an account", async () => {
+    const request: CreateAccountRequest = {
+      username: `username-${randomUUID()}`,
+      password: "password",
+    };
+    const account = await createAccount(request);
+    console.log({ account });
+    const gottenAccount = await getAccount(request);
+    assertEquals(account, gottenAccount);
+  });
 });
