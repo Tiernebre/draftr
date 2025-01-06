672a4b397eda93c91e366b211b462cb021fe287f
Brendan Tierney <tiernebre@gmail.com>
2025-01-06 00:09:07 +0000

    unused method

--- pages/lib/request.ts
+++ pages/lib/request.ts
@@ -5,11 +5,6 @@ export const formRequestToSchema = <T extends Record<string, unknown>>(
   request: Request,
 ) => request.formData().then(formDataToJson).then(schema.parse);
 
-const formRequestToJson = <T extends Record<string, unknown>>(
-  request: Request,
-): Promise<T | Record<string, unknown>> =>
-  request.formData().then(formDataToJson);
-
 const formDataToJson = <T extends Record<string, unknown>>(
   formData: FormData,
 ): T => Object.fromEntries(formData.entries()) as T;
