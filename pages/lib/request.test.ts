144210fc5225568b3b1230735f30f2a2c858bb15
Brendan Tierney <tiernebre@gmail.com>
2025-01-06 00:01:00 +0000

    schema parse

--- pages/lib/request.test.ts
+++ pages/lib/request.test.ts
@@ -1,13 +1,18 @@
 import { assertEquals } from "@std/assert";
 import { METHOD } from "@std/http/unstable-method";
-import { formRequestToJson } from "./request.ts";
+import { formRequestToSchema } from "./request.ts";
+import { createSessionRequestSchema } from "../../types/dto/session.ts";
 
-Deno.test("converts a form request data to json", async () => {
+Deno.test("converts a form request data to a zod schema", async () => {
   const formData = new FormData();
-  formData.append("key", "value");
+  formData.append("username", "username");
+  formData.append("password", "password");
   const request = new Request("http://0.0.0.0", {
     method: METHOD.Post,
     body: new URLSearchParams(formData as unknown as Record<string, string>),
   });
-  assertEquals(await formRequestToJson(request), { key: "value" });
+  assertEquals(await formRequestToSchema(createSessionRequestSchema, request), {
+    username: "username",
+    password: "password",
+  });
 });
--- pages/lib/request.ts
+++ pages/lib/request.ts
@@ -1,15 +1,15 @@
-import { ZodSchema } from "zod";
-
-export const formRequestToJson = <T extends Record<string, unknown>>(
-  request: Request,
-): Promise<T | Record<string, unknown>> =>
-  request.formData().then(formDataToJson);
-
-export const formDataToJson = <T extends Record<string, unknown>>(
-  formData: FormData,
-): T => Object.fromEntries(formData.entries()) as T;
+import { type ZodSchema } from "zod";
 
 export const formRequestToSchema = <T extends Record<string, unknown>>(
   schema: ZodSchema<T>,
   request: Request,
 ) => request.formData().then(formDataToJson).then(schema.parse);
+
+const formRequestToJson = <T extends Record<string, unknown>>(
+  request: Request,
+): Promise<T | Record<string, unknown>> =>
+  request.formData().then(formDataToJson);
+
+const formDataToJson = <T extends Record<string, unknown>>(
+  formData: FormData,
+): T => Object.fromEntries(formData.entries()) as T;
