cf4a728ca9e62c8b4efa95d5c672de834f693f63
Brendan Tierney <tiernebre@gmail.com>
2025-01-11 13:00:45 +0000

    all tests passing

--- pages/accounts/mod.test.ts
+++ pages/accounts/mod.test.ts
@@ -2,12 +2,23 @@ import { route } from "@std/http/unstable-route";
 import { routes } from "./mod.ts";
 import { DOMParser } from "@b-fuze/deno-dom";
 import { assert } from "@std/assert";
+import { afterAll, beforeAll, describe, it } from "@std/testing/bdd";
 
-Deno.serve(route(routes, () => new Response("Not Found")));
+describe("accounts page", () => {
+  let server: Deno.HttpServer;
 
-Deno.test("renders the form on GET", async () => {
-  const response = await fetch("http://localhost:8000/accounts");
-  const html = await response.text();
-  const doc = new DOMParser().parseFromString(html, "text/html");
-  assert(doc.querySelector("form"));
+  beforeAll(() => {
+    server = Deno.serve(route(routes, () => new Response("Not Found")));
+  });
+
+  it("renders the form on GET", async () => {
+    const response = await fetch("http://localhost:8000/accounts/");
+    const html = await response.text();
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    assert(doc.querySelector("form"));
+  });
+
+  afterAll(async () => {
+    await server.shutdown();
+  });
 });
