3d21e9e65bc5807859f2428c1cbc5dd7e94f725f
Brendan Tierney <tiernebre@gmail.com>
2025-02-01 17:13:54 +0000

    working initial navigation layout

--- pages/lib/page.ts
+++ pages/lib/page.ts
@@ -11,11 +11,20 @@ export const page = ({ head, body, title }: {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>${title}</title>
+  <link rel="stylesheet" href="common.css">
   ${head || ""}
 </head>
 
 <body>
+<header>
+  <nav>
+    <a class="home-link" href="/">Draftr</a>
+    <a href="/drafts/">Drafts</a>
+  </nav>
+</header>
+<main>
 ${body}
+</main>
 </body>
 </html>
     `,
